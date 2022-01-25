af509f863b34d6215e3ce4974451095179c705d8
Somdev Basu <somdevbasu100@gmail.com>
2022-01-25 16:05:46 +0530

    Implemented the find_weather function

---
+++ clu.py
@@ -0,0 +1,31 @@
+import argparse
+import datetime
+import sys
+
+import pyowm  # this is an openweathermaps based package used to access weather info
+from pyowm.uvindexapi30 import uv_client, uvindex # this package will be used to access details wrt the UV Index at a location
+
+
+def find_weather(args):
+    owm = pyowm.OWM("---------------------APIKEY-------------------")
+    mgr = owm.weather_manager()
+    observation = mgr.weather_at_place(args.city)
+    w = observation.weather
+    temp = w.temperature('celsius')
+    status = w.detailed_status
+    humidity = w.humidity
+    pressure = w.pressure['press']
+    temperature = str(int(temp['temp']))+" degrees Celcius"
+    windSpeed = str(w.wind()['speed'])
+    windDegree = str(w.wind()['deg'])
+    return "\nDetailed Weather Updates::: " + args.city.upper() + "\nHumidity: " + str(humidity) + "\nPressure: " + str(pressure) + "\nTemperature: " + str(temperature) + "\nWind Speed: " + windSpeed + "\nWind Degree: " + windDegree
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--city', type = str, default = "Bangalore", help = "This is an app that helps find weather info for a user request.")
+    #parser.add_argument('--o', type = str, default = "add", help = "THis is a utility for weather info")
+
+    args = parser.parse_args()
+
+    sys.stdout.write(str(find_weather(args))+ "\n\n")
